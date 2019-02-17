434d8fb2d01699ae1ad650300e9ab6cbbc5cab9a
Shaurya Bhandari <shauryabhandari25@gmail.com>
2019-02-17 20:39:36 +0530

    Add files via upload

---
+++ Random Forest/RF-SKL(Classification).py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("bill_authentication.csv")
+print (dataset.head())
+
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.ensemble import RandomForestClassifier
+
+classifier = RandomForestClassifier(n_estimators=20, random_state=0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
---
+++ Random Forest/RF-SKL(Regression).py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv('petrol_consumption.csv')
+
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.ensemble import RandomForestRegressor
+
+regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+from sklearn import metrics
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
